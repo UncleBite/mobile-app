51e2e2c0d9d8cccf29ada208c502b4579b4a277a
yl543 <yl543@duke.edu>
2019-09-10 15:16:57 -0400

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by yuqiao liang on 9/6/19.
+//  Copyright © 2019 yuqiao liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
