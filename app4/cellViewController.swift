60f92bd391b55d17ccca1535d83d30e2fd322bc9
yl543 <yl543@duke.edu>
2019-09-17 16:37:17 -0400

    add tableview

---
+++ app4/cellViewController.swift
@@ -0,0 +1,31 @@
+//
+//  cellViewController.swift
+//  app4
+//
+//  Created by yuqiao liang on 9/17/19.
+//  Copyright © 2019 yuqiao liang. All rights reserved.
+//
+
+import UIKit
+
+class cellViewController: UIViewController {
+
+    @IBOutlet weak var cellDetailLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
